7578168bfec22718a64123fb6333020b0d7535f1
adheindah <48880770+adheindah@users.noreply.github.com>
2019-03-24 18:34:48 +0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> information Retrieval UNISBANK </title>
+<style>
+	h1 {
+	color: blue;
+	text-align: center;
+	margin-top: 5%;
+}
+</style>
+</head>
+<body>
+<h1> <?php echo 'Hello World';?> </h1>
+<h1> <?php echo '16.01.55.0035';?> </h1>
+<h1> <?php echo 'Adhe Indah Cahyani';?> </h1>
+</body>
+</html>
